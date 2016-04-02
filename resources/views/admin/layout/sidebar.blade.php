bd9d4d63a9146d702f79b6d631886dec319d8806
bapppa <iftakharul.alam@live.com>
2016-04-02 14:11:25 +0600

    url segment working

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -1,10 +1,11 @@
 <div class="col-md-2 sB">
     <div class="list-group sidebar">
-        <a href="/" class="list-group-item active"><i class="fa fa-home"></i> Home</a>
+        <a href="/" class="list-group-item {{Request::segment(1)==''?'active':''}}"><i class="fa fa-home"></i> Home</a>
         <a href="#" class="list-group-item"><i class="fa fa-shopping-basket"></i> Sell Product</a>
-        <a href="product" class="list-group-item"><i class="fa fa-cubes"></i> Product</a>
+        <a href="product" class="list-group-item {{Request::segment(1)=='product'?'active':''}}">
+            <i class="fa fa-cubes"></i> Product</a>
         <hr/>
-        <a href="category" class="list-group-item"><i class="fa fa-building"></i> Category</a>
+        <a href="category" class="list-group-item {{Request::segment(1)=='category'?'active':''}}"><i class="fa fa-building"></i> Category</a>
         <hr>
         <a href="user" class="list-group-item"><i class="fa fa-users"></i> Users</a>
         <a href="#" class="list-group-item"><i class="fa fa-cogs"></i> Settings</a>
