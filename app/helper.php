157ba05f76b07cf8b5aadd764f9ba1cbcffdd071
Bappa <bappa2du@gmail.com>
2016-06-06 00:58:34 +0600

    update

--- app/helper.php
+++ app/helper.php
@@ -2,5 +2,6 @@
 
 function colors()
 {
-    return '#' . dechex(rand(0x999999, 0xDDDDDD));
+    $color = ['#4aa3df', '#2980b9', '#9b59b6', '#34495e', '#8e44ad', '#27ae60', '#8e44ad', '#1abc9c', '#d35400', '#e74c3c', '#c0392b'];
+    return $color[rand(0, 10)];
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,44 +6,49 @@
   <div class="col-md-8 col-md-offset-2">
     <div class="homcon">
   <div class="row">
-    <div class="col-md-2" style="background: {{colors()}}">
-      <a href="product" class="ball"><i class="fa fa-cube"></i><span>Product</span></a>
+    <div class="col-md-2 ball" style="background: {{colors()}}">
+      <div class="n">13</div>
+      <a href="product"><i class="fa fa-cube"></i><span>Product</span></a>
     </div>
-    <div class="col-md-2" style="background: {{colors()}}">
-      <a href="discount" class="ball"><i class="fa fa-crop"></i><span>Discount</span></a>
+    <div class="col-md-2 ball" style="background: {{colors()}}">
+      <a href="discount"><i class="fa fa-crop"></i><span>Discount</span></a>
     </div>
-    <div class="col-md-2" style="background: {{colors()}}">
-      <a href="brand" class="ball"><i class="ion-ios-star"></i><span>Brand</span></a>
+    <div class="col-md-2 ball" style="background: {{colors()}}">
+      <div class="n">7</div>
+      <a href="brand"><i class="ion-ios-star"></i><span>Brand</span></a>
     </div>
-    <div class="col-md-2" style="background: {{colors()}}">
-      <a href="report" class="ball"><i class="ion-ios-browsers"></i><span>Report</span></a>
+    <div class="col-md-2 ball" style="background: {{colors()}}">
+      <div class="n">7</div>
+      <a href="report"><i class="ion-ios-browsers"></i><span>Report</span></a>
     </div>
-    <div class="col-md-2" style="background: {{colors()}}">
-      <a href="analysis" class="ball"><i class="ion ion-easel"></i><span>Analysis</span></a>
+    <div class="col-md-2 ball" style="background: {{colors()}}">
+      <a href="analysis"><i class="ion ion-easel"></i><span>Analysis</span></a>
     </div>
-    <div class="col-md-2" style="background: {{colors()}}">
-      <a href="purchase" class="ball"><i class="ion-briefcase"></i><span>Purchase</span></a>
+    <div class="col-md-2 ball" style="background: {{colors()}}">
+      <a href="purchase"><i class="ion-briefcase"></i><span>Purchase</span></a>
     </div>
-    <div class="col-md-2" style="background: {{colors()}}">
-      <a href="sell" class="ball"><i class="ion-ios-cart-outline"></i><span>Sell</span></a>
+    <div class="col-md-2 ball" style="background: {{colors()}}">
+      <a href="sell"><i class="ion-ios-cart-outline"></i><span>Sell</span></a>
     </div>
-    <div class="col-md-2" style="background: {{colors()}}">
-      <a href="sells-history" class="ball"><i class="ion-ios-paper-outline"></i><span>History</span></a>
+    <div class="col-md-2 ball" style="background: {{colors()}}">
+      <a href="sells-history"><i class="ion-ios-paper-outline"></i><span>History</span></a>
     </div>
-    <div class="col-md-2" style="background: {{colors()}}">
-      <a href="stock" class="ball"><i class="fa fa-cubes"></i><span>Stock</span></a>
+    <div class="col-md-2 ball" style="background: {{colors()}}">
+      <div class="n">23</div>
+      <a href="stock"><i class="fa fa-cubes"></i><span>Stock</span></a>
     </div>
-    <div class="col-md-2" style="background: {{colors()}}">
-      <a href="refund" class="ball"><i class="ion-ios-redo"></i><span>Refund</span></a>
+    <div class="col-md-2 ball" style="background: {{colors()}}">
+      <a href="refund"><i class="ion-ios-redo"></i><span>Refund</span></a>
     </div>
-    <div class="col-md-2" style="background: {{colors()}}">
-      <a href="customer" class="ball"><i class="ion-android-people"></i><span>Customer</span></a>
+    <div class="col-md-2 ball" style="background: {{colors()}}">
+      <a href="customer"><i class="ion-android-people"></i><span>Customer</span></a>
     </div>
-    <div class="col-md-2" style="background: {{colors()}}">
-      <a href="settings" class="ball"><i class="fa fa-cogs"></i><span>Setting</span></a>
+    <div class="col-md-2 ball" style="background: {{colors()}}">
+      <a href="settings"><i class="fa fa-cogs"></i><span>Setting</span></a>
     </div>
-    <div class="col-md-2" style="background: {{colors()}}">
-      <a href="user" class="ball"><i class="fa fa-users"></i><span>User</span></a>
+    <div class="col-md-2 ball" style="background: {{colors()}}">
+      <div class="n">23</div>
+      <a href="user"><i class="fa fa-users"></i><span>User</span></a>
     </div>
   </div>
 </div>
